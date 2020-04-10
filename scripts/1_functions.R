fe02af5ce75a33d2ae8ee0939fbcf193797a4454
jcvdav <juancarlos.villader@gmail.com>
2020-04-10 11:20:31 -0700

    Updated scripts

--- scripts/1_functions.R
+++ scripts/1_functions.R
@@ -5,19 +5,48 @@ which_max_n <- function(vector, proportion) {
   vector %in% head(sort(vector, decreasing = T), n)
 }
 
+sample_patch <- function(vector, proportion) {
+  n <- proportion * length(vector)
+  vector %in% sample(vector, size = n, replace = F)
+}
+
+top_n <- function(matrix, p)
+
 # Define wrapper that conserves a given proportion
-conserve <- function(matrix, proportion) {
+conserve <- function(matrix, proportion, tactic) {
   
-  # Call the function to match the max n in each column
-  conserved <- apply(X = benefit, MARGIN = 2, FUN = which_max_n, proportion)
+  # How are we conserving?
+  if(tactic == "bau"){
+    
+    # Call the function to match the max n in each column
+    conserved <- apply(X = matrix, MARGIN = 2, FUN = which_max_n, proportion)
+    
+  } else if (tactic == "random_c"){
+    
+    # Random patch-level conservation
+    conserved <- apply(X = matrix, MARGIN = 2, FUN = sample_patch, proportion)
+    
+  } else if (tactic == "random_g") {
+    
+    # Random global conservation
+    n <- proportion * length(matrix)
+    conserved <- matrix %in% sample(x = matrix, size = n, replace = F)
+    
+  } else if(tactic == "mkt") {
+    
+    # market approach here
+    n <- proportion * length(matrix)
+    conserved <- matrix %in% head(sort(matrix, decreasing = T), n)
+  }
+    
   
   # Return the results
   return(conserved)
 }
 
-global_benefits <- function(matrix, proportion){
-  conserved <- conserve(matrix = benefit, proportion = proportion)
-  global_benefits <- sum(conserved * benefit)
+global_benefits <- function(matrix, proportion, tactic = "bau"){
+  conserved <- conserve(matrix = matrix, proportion = proportion, tactic = tactic)
+  global_benefits <- sum(conserved * matrix)
   
   return(global_benefits)
 }
--- scripts/2_scenario_bau.R
+++ scripts/2_scenario_bau.R
@@ -1,9 +1,11 @@
+# Load libraries
+library(tidyverse)
 
 # Set up a 10 by 10 matrix
 # Each column represents a country
 
 props <- seq(0.1, 1, by = 0.1)
-gb <- matrix(nrow = 10, ncol = 100)
+gb_bau <- gb_ran_c <- gb_ran_g <- gb_mkt <- matrix(nrow = 10, ncol = 100)
 
 for (k in 1:100) {
   
@@ -12,19 +14,35 @@ for (k in 1:100) {
   
   j <- 1
   for (i in props){
-    gb[j, k] <- global_benefits(benefit, proportion = i)
+    gb_bau[j, k] <- global_benefits(matrix = benefit, proportion = i, tactic = "bau")
+    gb_ran_c[j, k] <- global_benefits(matrix = benefit, proportion = i, tactic = "random_c")
+    gb_ran_g[j, k] <- global_benefits(matrix = benefit, proportion = i, tactic = "random_g")
+    gb_mkt[j, k] <- global_benefits(matrix = benefit, proportion = i, tactic = "mkt")
     j <- j + 1
   }
   
   
 }
 
-colnames(gb) <- paste0("iter", 1:100)
+gb_bau_df <- tibble(prop = props) %>% 
+  cbind(gb_bau) %>% 
+  mutate(tactic = "bau")
 
-tibble(prop = props) %>% 
-  cbind(gb) %>% 
-  gather(iteration, value, -prop) %>% 
-  ggplot(aes(x = prop, y = value)) +
+gb_ran_c <- tibble(prop = props) %>% 
+  cbind(gb_ran_c) %>% 
+  mutate(tactic = "ran_c")
+
+gb_ran_g <- tibble(prop = props) %>% 
+  cbind(gb_ran_g) %>% 
+  mutate(tactic = "ran_g")
+
+gb_mkt <- tibble(prop = props) %>% 
+  cbind(gb_mkt) %>% 
+  mutate(tactic = "mkt")
+
+rbind(gb_bau_df, gb_ran_c, gb_ran_g, gb_mkt) %>% 
+  gather(iteration, value, -c(prop, tactic)) %>% 
+  ggplot(aes(x = prop, y = value, color = tactic)) +
   geom_point() +
   stat_summary(geom = "ribbon", fun.data = mean_sdl, alpha = 0.5) +
   stat_summary(geom = "line", fun = "mean")
