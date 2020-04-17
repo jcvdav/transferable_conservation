a49fd2c6fc8285f6803b0721fc1e39f8f1f31866
Juan Carlos Villasenor Derbez <=>
2020-04-17 14:41:36 -0700

    Updated readme with TOO list

--- README.md
+++ README.md
@@ -20,6 +20,31 @@
 -- transferable_conservation.Rproj
 ```
 
+---------
+# Goals
+## Get three or four levels:
+- Global
+- Ocean basins
+- Eco / Bio regions
+- Within an EEZ wiht TURFs
+
+## Calculate what the allocations are for each
+- Percent required
+- Absolute numbers required
+
+## Assign value to each parcel
+Will it be based on number of species present? Ecological distintiveness...
+
+# Explicit TO-DOs
+
+- Get shapefiles:
+   - EEZs
+   - Spaldings bioregons
+   - Ocean basins
+   - Habitat maps
+
+- Get obligations for each country
+
 --------- 
 
 <a href="https://orcid.org/0000-0003-1245-589X" target="orcid.widget" rel="noopener noreferrer" style="vertical-align:top;"><img src="https://orcid.org/sites/default/files/images/orcid_16x16.png" style="width:1em;margin-right:.5em;" alt="ORCID iD icon">orcid.org/0000-0003-1245-589X</a>
--- scripts/2_scenario_bau.R
+++ scripts/2_scenario_bau.R
@@ -1,22 +1,22 @@
 # Load libraries
+library(startR)
 library(tidyverse)
 
 # Set up a 10 by 10 matrix
 # Each column represents a country
 
 props <- seq(0.1, 1, by = 0.1)
-gb_bau <- gb_ran_c <- gb_ran_g <- gb_mkt <- matrix(nrow = 10, ncol = 100)
+gb_bau <- gb_ran_c <- gb_mkt <- matrix(nrow = 10, ncol = 100)
 
 for (k in 1:100) {
   
-  benefit <- matrix(runif(n = 100, min = 0, max = 100) ^ 2.5, ncol = 10)
+  benefit <- t(seq(0.5, 5, by = 0.5) * t(matrix(runif(n = 100, min = 0, max = 10) ^ 2.5, ncol = 10)))
   colnames(benefit) <- LETTERS[1:10]
   
   j <- 1
   for (i in props){
     gb_bau[j, k] <- global_benefits(matrix = benefit, proportion = i, tactic = "bau")
     gb_ran_c[j, k] <- global_benefits(matrix = benefit, proportion = i, tactic = "random_c")
-    gb_ran_g[j, k] <- global_benefits(matrix = benefit, proportion = i, tactic = "random_g")
     gb_mkt[j, k] <- global_benefits(matrix = benefit, proportion = i, tactic = "mkt")
     j <- j + 1
   }
@@ -28,23 +28,21 @@ gb_bau_df <- tibble(prop = props) %>%
   cbind(gb_bau) %>% 
   mutate(tactic = "bau")
 
-gb_ran_c <- tibble(prop = props) %>% 
+gb_ran_c_df <- tibble(prop = props) %>% 
   cbind(gb_ran_c) %>% 
   mutate(tactic = "ran_c")
 
-gb_ran_g <- tibble(prop = props) %>% 
-  cbind(gb_ran_g) %>% 
-  mutate(tactic = "ran_g")
-
-gb_mkt <- tibble(prop = props) %>% 
+gb_mkt_df <- tibble(prop = props) %>% 
   cbind(gb_mkt) %>% 
   mutate(tactic = "mkt")
 
-rbind(gb_bau_df, gb_ran_c, gb_ran_g, gb_mkt) %>% 
+rbind(gb_bau_df, gb_ran_c_df, gb_mkt_df) %>% 
   gather(iteration, value, -c(prop, tactic)) %>% 
-  ggplot(aes(x = prop, y = value, color = tactic)) +
-  geom_point() +
-  stat_summary(geom = "ribbon", fun.data = mean_sdl, alpha = 0.5) +
-  stat_summary(geom = "line", fun = "mean")
+  ggplot(aes(x = prop, y = value, color = tactic, fill = tactic)) +
+  # stat_summary(geom = "ribbon", fun.data = mean_sdl, alpha = 0.25, color = "transparent") +
+  stat_summary(geom = "line", fun = "mean") +
+  labs(x = "Proportion of habitat conserved",
+       y = "Total benefit") +
+  ggtheme_plot()
 
 
