d5af7663b031d653b193b6331aa3363f7186dde3
Juan Carlos Villasenor Derbez <=>
2020-04-22 17:33:19 -0700

    Updating third script

--- scripts/01_cleaning/03_intersect_eez_and_meow.R
+++ scripts/01_cleaning/03_intersect_eez_and_meow.R
@@ -13,10 +13,11 @@
 # Load packages
 library(here)
 library(janitor)
+library(lwgeom)
 library(units)
 library(sf)
 library(tidyverse)
-library(lwgeom)
+
 
 # Load shapefiles
 eez <- st_read(here("data", "clean_world_eez_v11.gpkg"))  # Load clean EEZ
@@ -38,22 +39,35 @@ eez_meow <- st_intersection(eez, meow) %>%                # Intersect features
   mutate(ecoregion_div_eez = area_ecoregion / area_eez,  # Calculate ratio of polygon area to EEZ area
          province_div_eez = area_province / area_eez,
          realm_div_eez = area_realm / area_eez) %>% 
-  mutate_at(.vars = vars(ecoregion_div_eez,              # Convert to numeric
-                         province_div_eez,
-                         realm_div_eez),
-            .funs = drop_units) %>% 
+  # mutate_at(.vars = vars(ecoregion_div_eez,              # Convert to numeric
+                         # province_div_eez,
+                         # realm_div_eez),
+            # .funs = drop_units) %>% 
   select(mrgid, geoname, iso_3, area_eez,
          ecoregion, eco_code, area_ecoregion, ecoregion_div_eez,
          province, pro_code, area_province, province_div_eez,
          realm, rlm_code, area_realm, realm_div_eez)
 
-mapview::mapview(select(eez_meow, ecoregion_div_eez))
+# mapview::mapview(select(eez_meow, ecoregion_div_eez))
 
-eez_meow %>%
-  ggplot(aes(x = realm, y = ecoregion_div_eez, fill = province, color = ecoregion)) + 
-  geom_col(size = 1) +
-  scale_fill_viridis_d() +
-  startR::ggtheme_plot() +
-  facet_wrap(~ iso_3, scales = "free_x") +
-  theme(legend.position = "none")
+
+## Export
+# File name
+intersected_eez_and_meow_fn <- here("data", "intersected_eez_and_meow.gpkg")
+
+# Remove any existing files
+file.remove(intersected_eez_and_meow_fn)
+
+# Write geopackage
+st_write(obj = eez_meow, dsn = intersected_eez_and_meow_fn)
+
+
+
+
+
+
+
+
+
+
 
