ecac7e6021945e3f7d17d104f06ee2e873d099c4
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2020-07-14 11:42:04 -0700

    Moved file and updated to include rasterization of MEOWs

---
+++ scripts/02_processing/01_rasterize_eez_and_meow.R
@@ -0,0 +1,33 @@
+library(raster)
+library(fasterize)
+library(tidyverse)
+
+# Load reference raster
+benefits <- raster(file.path(ng_data_path, "03_output/05_spp_wts_smts_provs_MPAs/ranking_raster.tif"))
+
+# Load the vector data
+eez_meow <- st_read(file.path(project_path, "data", "intersected_eez_and_meow.gpkg")) %>% 
+  select(iso3n, contains("code")) 
+
+# Rasterize
+eez_raster <- fasterize(eez_meow, benefits, field = "iso3n")
+realm_raster <- fasterize(eez_meow, benefits, field = "rlm_code")
+province_raster <- fasterize(eez_meow, benefits, field = "pro_code")
+ecoregion_raster <- fasterize(eez_meow, benefits, field = "eco_code")
+
+
+# Export rasters
+writeRaster(x = eez_raster,
+            filename = file.path(project_path, "data", "eez_raster.tif"),
+            overwrite = TRUE)
+writeRaster(x = realm_raster,
+            filename = file.path(project_path, "data", "rlm_raster.tif"),
+            overwrite = TRUE)
+writeRaster(x = province_raster,
+            filename = file.path(project_path, "data", "pro_raster.tif"),
+            overwrite = TRUE)
+writeRaster(x = ecoregion_raster,
+            filename = file.path(project_path, "data", "eco_raster.tif"),
+            overwrite = TRUE)
+
+# END OF SCRIPT #
