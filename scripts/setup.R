f113e1bcf7fa2e5349412a530b8da5387a7a9b0e
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2020-07-14 11:40:13 -0700

    Updated setu pscript

--- scripts/setup.R
+++ scripts/setup.R
@@ -4,9 +4,9 @@
 # Check for OS
 sys_path <- ifelse(Sys.info()["sysname"]=="Windows", "~/G:/","~/Google Drive File Stream")
 # Nat geo ocean conservation priorities path
-ng_data_path <- paste0(sys_path,"Shared drives/emlab/projects/current-projects/ocean-conservation-priorities/data")
+ng_data_path <- file.path(sys_path,"Shared drives/emlab/projects/current-projects/ocean-conservation-priorities/data")
 # Path to our emLab's data folder
-data_path <- paste0(sys_path,"Shared drives/emlab/data/")
+data_path <- file.path(sys_path,"Shared drives/emlab/data/")
 # Path to this projects directory
 project_path <- "~/Google Drive File Stream/Shared drives/emlab/projects/current-projects/transferable-conservation"
 
@@ -20,4 +20,4 @@ proj_longlat <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs "             
 
 # A color palete
 zis_pal <- wesanderson::wes_palette("Zissou1", 100, type = "continuous")
-mat_pal <- colorRamps::matlablike(100)
+mat_pal <- colorRamps::matlab.like(100)
