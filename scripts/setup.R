7d6d31c25762cad058401af4fd7d786f91a12620
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2020-07-14 11:31:45 -0700

    Updated renv dependencies and setup script

--- scripts/setup.R
+++ scripts/setup.R
@@ -1,4 +1,23 @@
+##########################
+## Paths to directories ##
+##########################
+# Check for OS
+sys_path <- ifelse(Sys.info()["sysname"]=="Windows", "~/G:/","~/Google Drive File Stream")
+# Nat geo ocean conservation priorities path
+ng_data_path <- paste0(sys_path,"Shared drives/emlab/projects/current-projects/ocean-conservation-priorities/data")
+# Path to our emLab's data folder
+data_path <- paste0(sys_path,"Shared drives/emlab/data/")
+# Path to this projects directory
 project_path <- "~/Google Drive File Stream/Shared drives/emlab/projects/current-projects/transferable-conservation"
-ng_data_path <- "~/Google Drive File Stream/Shared drives/emlab/projects/current-projects/ocean-conservation-priorities/data"
-proj_moll <- "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs "
-proj_longlat <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs "
+
+
+###########################
+## Useful general values ##
+###########################
+# proj4strings
+proj_moll <- "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs " # Mollweide - EPSG code ESRI:54009
+proj_longlat <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs "              # Unprojected coordinates - EPSG code 4326
+
+# A color palete
+zis_pal <- wesanderson::wes_palette("Zissou1", 100, type = "continuous")
+mat_pal <- colorRamps::matlablike(100)
