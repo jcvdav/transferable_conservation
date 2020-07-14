302e64366a2b2a908eb60476986a283cecc69e71
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2020-07-14 11:45:29 -0700

    Updated data path

--- scripts/02_processing/01_rasterize_eez_and_meow.R
+++ scripts/02_processing/01_rasterize_eez_and_meow.R
@@ -6,7 +6,7 @@ library(tidyverse)
 benefits <- raster(file.path(ng_data_path, "03_output/05_spp_wts_smts_provs_MPAs/ranking_raster.tif"))
 
 # Load the vector data
-eez_meow <- st_read(file.path(project_path, "data", "intersected_eez_and_meow.gpkg")) %>% 
+eez_meow <- st_read(file.path(project_path, "processed_data", "intersected_eez_and_meow.gpkg")) %>% 
   select(iso3n, contains("code")) 
 
 # Rasterize
@@ -18,16 +18,16 @@ ecoregion_raster <- fasterize(eez_meow, benefits, field = "eco_code")
 
 # Export rasters
 writeRaster(x = eez_raster,
-            filename = file.path(project_path, "data", "eez_raster.tif"),
+            filename = file.path(project_path, "processed_data", "eez_raster.tif"),
             overwrite = TRUE)
 writeRaster(x = realm_raster,
-            filename = file.path(project_path, "data", "rlm_raster.tif"),
+            filename = file.path(project_path, "processed_data", "rlm_raster.tif"),
             overwrite = TRUE)
 writeRaster(x = province_raster,
-            filename = file.path(project_path, "data", "pro_raster.tif"),
+            filename = file.path(project_path, "processed_data", "pro_raster.tif"),
             overwrite = TRUE)
 writeRaster(x = ecoregion_raster,
-            filename = file.path(project_path, "data", "eco_raster.tif"),
+            filename = file.path(project_path, "processed_data", "eco_raster.tif"),
             overwrite = TRUE)
 
 # END OF SCRIPT #
