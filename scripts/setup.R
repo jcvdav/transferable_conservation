e1e7246f5dc39154520c9d917c8f4e4d0d5cd7d3
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2020-07-14 15:41:40 -0700

    Updating setup scrip to include default geometries

--- scripts/setup.R
+++ scripts/setup.R
@@ -19,5 +19,22 @@ proj_moll <- "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs "
 proj_longlat <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs "              # Unprojected coordinates - EPSG code 4326
 
 # A color palete
-zis_pal <- wesanderson::wes_palette("Zissou1", 100, type = "continuous")
+zis_con <- wesanderson::wes_palette("Zissou1", 100, type = "continuous")
 mat_pal <- colorRamps::matlab.like(100)
+
+
+# Default geometry colors, fills, and shapes
+update_geom_defaults(geom = "col", list(color = "black",
+                                        fill = "steelblue"))
+
+update_geom_defaults(geom = "point", list(color = "black",
+                                        fill = "steelblue",
+                                        shape = 21,
+                                        size = 3))
+
+update_geom_defaults(geom = "line", list(color = "steelblue"))
+
+update_geom_defaults(geom = "sf", list(color = "black",
+                                        fill = "transparent",
+                                       size = 0.3))
+l
