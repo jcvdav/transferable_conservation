06c66bba53be742fd8d68ad6c9b0a60b44c02b45
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2020-07-14 15:41:20 -0700

    Updated cost and benefits script to export data

--- scripts/02_processing/02_get_costs_and_benefits.R
+++ scripts/02_processing/02_get_costs_and_benefits.R
@@ -1,7 +1,6 @@
 # Load packages
 library(startR)
 library(raster)
-library(rmapshaper)
 library(sf)
 library(tidyverse)
 
@@ -12,27 +11,32 @@ benefits_raster <- raster(file.path(ng_data_path, "03_output/05_spp_wts_smts_pro
 # Costs raster
 costs_raster <- -1 * raster(file.path(ng_data_path, "03_output/07_food/delta_v_raster_a2.tif"))
 
-# EEZ_MEOW raster
-eez_meow_raster <- raster(file.path(project_path, "data", "eez_meow_raster.tif"))
+# Load rasters
+iso3n <- raster(file.path(project_path, "processed_data", "eez_raster.tif"))
+rlm_code <- raster(file.path(project_path, "processed_data", "rlm_raster.tif"))
+pro_code <- raster(file.path(project_path, "processed_data", "pro_raster.tif"))
+eco_code <- raster(file.path(project_path, "processed_data", "eco_raster.tif"))
 
-eez_meow <- st_read(file.path(project_path, "data", "intersected_eez_and_meow.gpkg"))
+eez_meow <- st_read(file.path(project_path, "processed_data", "intersected_eez_and_meow.gpkg"))
 
 # Extract codes for each pixel
-cb <- stack(eez_meow_raster, benefits_raster, costs_raster) %>% 
+cb <- stack(iso3n, rlm_code, pro_code, eco_code, benefits_raster, costs_raster) %>% 
   as.data.frame(xy = T) %>% 
-  rename(lon = x, lat = y, iso3n = eez_meow_raster) %>% 
+  rename(lon = x, lat = y,
+         iso3n = eez_raster, rlm_code = rlm_raster, pro_code = pro_raster, eco_code = eco_raster,
+         benefit = ranking_raster,
+         cost = delta_v_raster_a2) %>% 
   drop_na()
 
 # Create a master dataset with all the metadata for eahc pixel
 master_data <- eez_meow %>% 
-  # filter(iso3 %in% c("CAN")) %>%
   st_drop_geometry() %>% 
-  select(iso3, iso3n, ecoregion, province, realm) %>% 
-  distinct() %>% 
-  left_join(cb, by = "iso3n") %>% 
-  select(lon, lat, iso3, ecoregion, province, realm, benefit = ranking_raster, cost = delta_v_raster_a2) %>% 
+  select(iso3, ecoregion, province, realm, iso3n, contains("code")) %>% 
+  left_join(cb, by = c("iso3n", "rlm_code", "pro_code", "eco_code")) %>% 
+  select(lon, lat, iso3, ecoregion, province, realm, benefit, cost) %>% 
   mutate(mb = benefit / cost,
-         neg = mb >= 0)
+         neg = mb >= 0) %>% 
+  drop_na(lat, lon)
 
 global_data <- master_data %>% 
   arrange(neg, desc(mb)) %>%
@@ -40,7 +44,7 @@ global_data <- master_data %>%
          tc = cumsum(cost),
          pct = (1:nrow(.)) / nrow(.))
   
-country_data <- master_data %>% 
+eez_data <- master_data %>% 
   group_by(iso3) %>% 
   arrange(neg, desc(mb)) %>%
   mutate(tb = cumsum(benefit),
@@ -48,7 +52,7 @@ country_data <- master_data %>%
          pct = (1:length(tc)) / length(tc)) %>% 
   ungroup()
 
-country_realm <- master_data %>% 
+rlm_eez <- master_data %>% 
   group_by(iso3, realm) %>% 
   arrange(neg, desc(mb)) %>%
   mutate(tb = cumsum(benefit),
@@ -56,7 +60,7 @@ country_realm <- master_data %>%
          pct = (1:length(tc)) / length(tc)) %>% 
   ungroup()
 
-country_province <- master_data %>% 
+pro_eez <- master_data %>% 
   group_by(iso3, province) %>% 
   arrange(neg, desc(mb)) %>%
   mutate(tb = cumsum(benefit),
@@ -64,13 +68,28 @@ country_province <- master_data %>%
          pct = (1:length(tc)) / length(tc)) %>% 
   ungroup()
 
-country_ecoregion <- master_data %>% 
+eco_eez <- master_data %>% 
   group_by(iso3, ecoregion) %>% 
   arrange(neg, desc(mb)) %>%
   mutate(tb = cumsum(benefit),
          tc = cumsum(cost),
          pct = (1:length(tc)) / length(tc)) %>% 
   ungroup()
+
+# Export the data
+saveRDS(master_data,
+        file = file.path(project_path, "processed_data", "master_costs_and_benefits.rds"))
+saveRDS(global_data,
+        file = file.path(project_path, "processed_data", "global_costs_and_benefits.rds"))
+saveRDS(eez_data,
+        file = file.path(project_path, "processed_data", "eez_costs_and_benefits.rds"))
+
+saveRDS(rlm_eez,
+        file = file.path(project_path, "processed_data", "rlm_eez_costs_and_benefits.rds"))
+saveRDS(pro_eez,
+        file = file.path(project_path, "processed_data", "pro_eez_costs_and_benefits.rds"))
+saveRDS(eco_eez,
+        file = file.path(project_path, "processed_data", "eco_eez_costs_and_benefits.rds"))
 
 
 
