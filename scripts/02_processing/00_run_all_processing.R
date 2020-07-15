32839020f37b06fe981bc710fac588f04114416b
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2020-07-15 14:36:53 -0700

    typo

--- scripts/02_processing/00_run_all_processing.R
+++ scripts/02_processing/00_run_all_processing.R
@@ -4,7 +4,7 @@ library(here)
 
 source(file = here("scripts", "00_setup.R"), local = F)
 
-# CLEANING SCRIPT
+# PROCESSING SCRIPTS
 source(file = here("scripts", "02_processing", "01_rasterize_eez_and_meow.R"), local = F)
 source(file = here("scripts", "02_processing", "02_get_costs_and_benefits.R"), local = F)
 
