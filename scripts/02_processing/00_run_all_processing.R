0811ab2ee9574345acf6b5821c542274c501091d
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2020-07-22 11:17:36 -0700

    Updated run all processing script

--- scripts/02_processing/00_run_all_processing.R
+++ scripts/02_processing/00_run_all_processing.R
@@ -6,5 +6,6 @@ source(file = here("scripts", "00_setup.R"), local = F)
 
 # PROCESSING SCRIPTS
 source(file = here("scripts", "02_processing", "01_rasterize_eez_and_meow.R"), local = F)
-source(file = here("scripts", "02_processing", "02_get_costs_and_benefits.R"), local = F)
+source(file = here("scripts", "02_processing", "02_rasterize_costs.R"), local = F)
+source(file = here("scripts", "02_processing", "03_get_costs_and_benefits.R"), local = F)
 
