d3233f346cb7d9449700f324392308f69a670042
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2021-07-16 12:35:15 -0700

    Updated anakysis for 30% cons at province

--- scripts/04_analysis/06_province_level_market.R
+++ scripts/04_analysis/06_province_level_market.R
@@ -33,35 +33,42 @@ pro_h_sum <- readRDS(
 coast <- ne_countries(returnclass = "sf") %>% 
   st_transform(crs = epsg_moll) 
 
+# Load EEZ geopackage
+eez <- st_read(
+  file.path(project_path,
+            "processed_data",
+            "intersected_eez_meow_hem.gpkg")
+) %>% 
+  mutate(has_obligations = iso3 %in% unique(pro_eez_cb$iso3)) %>%
+  rmapshaper::ms_simplify(keep_shapes = T, sys = T) %>%
+  st_make_valid() %>%
+  filter(iso3 %in% unique(pro_eez_cb$iso3))
+
+conserving_nations <- pro_eez_cb %>% 
+  select(iso3, province) %>% 
+  distinct()
+
 # PROCESSING ############################################################################################
 
-# Get targets
-pro_conservation_target <- pro_eez_cb %>%
-  group_by(province) %>%
-  nest() %>%
-  mutate(target = map_dbl(data, benefit, 0.3)) %>%
-  select(-data)
 
 pro_trading_prices <- pro_h_sum %>% 
   group_by(province) %>% 
-  nest() %>% 
-  left_join(pro_conservation_target, by = "province") %>% 
-  mutate(trading_price = map2_dbl(.x = target, .y = data, get_trading_price)) %>% 
-  select(-data)
-
+  filter(pct_area <= 0.3) %>% 
+  slice_max(mc) %>% 
+  ungroup() %>% 
+  select(province, trading_price = mc)
 ## Identify conserved patches
 # Get the most efficient trading price
 
 # Filter to keep only the protected places
 bau <- pro_eez_cb %>% 
-  filter(pct <= 0.3) %>%                       # Keep the most efficient 30% of each country - province
+  filter(pct_area <= 0.3) %>%                       # Keep the most efficient 30% of each country - province
   mutate(approach = "bau")
 
 mkt <- pro_eez_cb %>% 
   left_join(pro_trading_prices, by = c("province")) %>%
   filter(mc <= trading_price) %>%              # Keep all patches in each country with a cost < trading price
-  mutate(approach = "mkt") %>% 
-  select(-c(trading_price,))
+  mutate(approach = "mkt")
 
 ## Calculate country-level summaries
 # For BAU
@@ -75,11 +82,23 @@ realized_bau_cb <- bau %>%
 
 # For a market
 realized_mkt_cb <- mkt %>% 
-  group_by(approach, iso3, province) %>% 
+  group_by(approach, iso3, province, trading_price) %>% 
   summarize(mkt_tb = sum(benefit, na.rm = T),
             mkt_tc = sum(cost, na.rm = T),
             mkt_area = n()) %>% 
   ungroup()
+
+#
+combined_outcomes <- conserving_nations %>% 
+  left_join(realized_mkt_cb, by = c("iso3", "province")) %>%
+  left_join(realized_bau_cb, by = c("iso3", "province")) %>% 
+  replace_na(replace = list(
+    mkt_tb = 0, mkt_tc = 0, mkt_area = 0,
+    bau_tb = 0, bau_tc = 0, bau_area = 0)) %>% 
+  select(-contains("app")) %>% 
+  mutate(mkt_tc2 = mkt_tc + ((bau_tb - mkt_tb) * trading_price),
+         mkt_tc = ifelse(mkt_tc2 <=0, bau_tc, mkt_tc2)) %>% 
+  select(-mkt_tc2)
 
 # Find stopping prices
 stops <- full_join(realized_mkt_cb, realized_bau_cb, by = c("iso3", "province")) %>% 
@@ -100,23 +119,34 @@ gains_from_trade <- full_join(realized_mkt_cb, realized_bau_cb, by = c("iso3", "
          ratio = mkt / bau)
 
 ## FIGURES #########################################################################
-got_paid <- full_join(realized_mkt_cb, realized_bau_cb, by = c("province", "iso3")) %>% 
-  select(-contains("app")) %>% 
-  mutate(gets_paid = bau_tc <= mkt_tc)
+got_paid <- combined_outcomes %>% 
+  mutate(gets_paid = mc_stop <= trading_price,
+         mkt_gains = bau_tc - mkt_tc) %>% 
+  select(iso3, province, gets_paid, bau_tc, mkt_tc, mkt_gains) %>% 
+  pivot_longer(cols = c(gets_paid, bau_tc, mkt_tc, mkt_gains), names_to = "variable", values_to = "value") %>% 
+  mutate(label = case_when(variable == "bau_tc" ~ "A) Business-as-usual",
+                           variable == "mkt_tc" ~ "B) Market-based (global)",
+                           variable == "mkt_gains" ~ "C) Gains from trade",
+                           T ~ NA_character_))
 
 # Plot the two states of the world
 two_states_map <-
-  rbind(bau, mkt %>% select(-target)) %>%
+  pro_eez_cb %>% 
+  select(lon, lat, benefit) %>% 
+  left_join(bau %>% select(lon, lat) %>% mutate(a = 1), by = c("lon", "lat")) %>% 
+  left_join(mkt %>% select(lon, lat) %>% mutate(b = 1), by = c("lon", "lat")) %>% 
+  mutate(status = case_when(a == 1 & b == 1 ~ "Protected anyway",
+                            a == 1 & is.na(b) ~ "Protected only under BAU",
+                            is.na(a) & b == 1 ~ "Protected only under market",
+                            is.na(a) & is.na(b) ~ NA_character_)) %>% 
+  drop_na(status) %>% 
   ggplot() +
-  geom_sf(data = coast) +
-  geom_raster(aes(x = lon, y = lat, fill = benefit)) +
-  facet_wrap(~approach, ncol = 1) +
+  geom_sf(data = coast, color = "black", fill = "transparent") +
+  geom_tile(aes(x = lon, y = lat, fill = status)) +
   ggtheme_map() +
-  scale_fill_viridis_c() +
-  guides(fill = guide_colorbar(title = "Biodiversity",
-                               frame.colour = "black",
-                               ticks.colour = "black")) +
-  labs(caption = "Both conservation strategies yield the same benefits,\nbut a market costs 18.3% less")
+  scale_fill_viridis_d(na.value = "gray") +
+  guides(fill = guide_legend(title = "Status")) +
+  theme(legend.position = "bottom")
 
 
 ## EXPORT FIGURES #########################################################################
@@ -154,23 +184,6 @@ gains_from_trade %>%
                caption = "Gains from trade from protecting 73.65 units of biodiversity. Difference shows BAU - Market, ratio shows Market / BAU.") %>% 
   # kableExtra::collapse_rows() %>% 
   cat(file = here::here("results", "tab", "pro_gains_from_trade.tex"))
-
-# Table of trading prices for each province
-totals <- pro_trading_prices %>% 
-  mutate(province = "Summary") %>% 
-  group_by(province) %>% 
-  summarize(trading_price = weighted.mean(trading_price, target),
-            target = sum(target)) %>% 
-  select(province, target, trading_price)
-
-pro_trading_prices %>% 
-  rbind(totals) %>% 
-  knitr::kable(format = "latex",
-               digits = 2,
-               col.names = c("province", "Biodiversity", "Trading Price"),
-               label = "pro-trading-prices",
-               caption = "Biodiversity targets and trading prices for 12 provinces. The Last row shows total biodiversity and weighted mean of trading price") %>% 
-  cat(file = here::here("results", "tab", "trading_prices_pro.tex"))
 
 
 
