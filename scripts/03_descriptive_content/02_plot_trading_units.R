543d123a03e7552dd2619c5d5b7a150468c6dfcb
Juan Carlos Villaseñor-Derbez <juancarlos@uccsb.edu>
2021-11-15 15:51:58 -0800

    Plotting trading units

--- scripts/03_descriptive_content/02_plot_trading_units.R
+++ scripts/03_descriptive_content/02_plot_trading_units.R
@@ -29,113 +29,159 @@ eez_meow <- st_read(file.path(project_path, "processed_data", "intersected_eez_a
 # Load a coastline
 coast <- ne_countries(returnclass = "sf")
 
+# Load hemisphere shapefile and intersect with EEZ
+hemisphere <- st_read(file.path(project_path, "processed_data", "hemispheres.gpkg")) %>% 
+  st_intersection(eez_meow)
+
 
 ## FIGURES #####################################################################
 
 # Global bubble
-ggplot() +
-  geom_sf(data = eez_meow, fill = "steelblue", color = "transparent") +
+global <- eez_meow %>% 
+  group_by(iso3) %>% 
+  summarize(a = 1) %>% 
+  ungroup() %>% 
+  ggplot() +
+  geom_sf(fill = "steelblue", color = "black") +
   geom_sf(data = coast, color = "black", size = 0.1) +
   ggtheme_map() +
-  theme(legend.position = "bottom")
+  ggtitle("Nations = 195") +
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0))
+
+lazy_ggsave(plot = global, filename = "trading_units/global",
+            width = 10, height = 5)
 
 
 # Hemisphere bubble
 
-hemisphere_map <- ggplot() +
+hemisphere_data <- hemisphere %>% 
+  group_by(hemisphere, iso3) %>% 
+  summarize(a = 1) %>% 
+  ungroup() %>% 
+  group_by(hemisphere) %>% 
+  mutate(n_eez = n_distinct(iso3)) %>% 
+  ungroup() %>% 
+  mutate(hemisphere = fct_reorder(hemisphere, n_eez))
+
+hemisphere_map <- 
+  ggplot() +
+  geom_sf(data = hemisphere_data, aes(fill = hemisphere), color = "black", size = 0.1) +
   geom_sf(data = coast, color = "black", size = 0.1) +
-  geom_sf(data = eez_meow, aes(fill = hemisphere), color = "black", size = 0.1) +
   ggtheme_map() +
   scale_fill_viridis_d() +
   labs(fill = "Hemisphere") +
-  theme(legend.position = "bottom")
+  theme(legend.position = "none") 
 
-
-
-
-lazy_ggsave(plot = hemisphere_map,
-            filename = "hemisphere_map",
-            width = 20, 
-            height = 10)
-
-
-# Map marine ecoregions
-realm_map <- ggplot() +
-  geom_sf(data = coast, color = "black", size = 0.1) +
-  geom_sf(data = eez_meow, aes(fill = realm), color = "black", size = 0.1) +
-  ggtheme_map() +
+hemisphere_bars <- hemisphere_data %>% 
+  st_drop_geometry() %>% 
+  select(hemisphere, n_eez) %>% 
+  distinct() %>% 
+  ggplot(aes(x = hemisphere, y = n_eez, fill = hemisphere)) +
+  geom_col() +
+  coord_flip() +
   scale_fill_viridis_d() +
-  labs(fill = "Realm") +
-  theme(legend.position = "bottom")
+  labs(x = "Hemisphere", y = "Number of nations") +
+  ggtheme_plot() +
+  theme(legend.position = "none") +
+  geom_hline(yintercept = 2, linetype = "dashed", color = "black")
+
+hemisphere_segments <- plot_grid(hemisphere_map, hemisphere_bars,
+                                 ncol = 1,
+                                 rel_heights = c(5, 2.5))
+
+lazy_ggsave(plot = hemisphere_segments,
+            filename = "trading_units/hemisphere",
+            width = 10,
+            height = 8)
 
 
-lazy_ggsave(plot = realm_map,
-            filename = "realm_map",
-            width = 20, 
-            height = 10)
+# Realm bubble
 
-# Provinces map
-
-provinces_map <- ggplot() +
-  geom_sf(data = coast, color = "black", size = 0.1) +
-  geom_sf(data = eez_meow, aes(fill = province), color = "black", size = 0.1) +
-  ggtheme_map() +
-  scale_fill_viridis_d() +
-  # labs(fill = "Realm") +
-  theme(legend.position = "None")
-
-lazy_ggsave(plot = provinces_map,
-            filename = "provinces_map",
-            width = 20, 
-            height = 10)
-
-# How many countries per MEOW?
-eezs_per_realm <- eez_meow %>% 
-  st_drop_geometry() %>%
+realm_data <- eez_meow %>% 
+  group_by(iso3, realm) %>% 
+  summarize(a = 1) %>% 
+  ungroup() %>% 
   group_by(realm) %>% 
-  summarize(n_eez = n_distinct(iso3)) %>% 
+  mutate(n_eez = n_distinct(iso3)) %>% 
   ungroup() %>% 
   mutate(realm = fct_reorder(realm, n_eez))
 
-eezs_per_province <- eez_meow %>% 
+realm_map <- 
+  ggplot() +
+  geom_sf(data = realm_data, aes(fill = realm), color = "black", size = 0.1) +
+  geom_sf(data = coast, color = "black", size = 0.1) +
+  ggtheme_map() +
+  scale_fill_viridis_d() +
+  labs(fill = "Realm") +
+  theme(legend.position = "none")
+
+realm_bars <- realm_data %>% 
   st_drop_geometry() %>% 
-  group_by(realm, province) %>% 
-  summarize(n_eez = n_distinct(iso3)) %>% 
+  select(realm, n_eez) %>% 
+  distinct() %>% 
+  ggplot(aes(x = realm, y = n_eez, fill = realm)) +
+  geom_col() +
+  coord_flip() +
+  labs(x = "Realm", y = "Number of nations") +
+  ggtheme_plot() +
+  theme(legend.position = "none") +
+  scale_fill_viridis_d() +
+  geom_hline(yintercept = 2, linetype = "dashed", color = "black")
+
+realm_segments <- plot_grid(realm_map, realm_bars,
+                            ncol = 1,
+                            rel_heights = c(5, 6.5))
+
+
+lazy_ggsave(plot = realm_segments,
+            filename = "trading_units/realm",
+            width = 10,
+            height = 12)
+
+# Provinces map
+
+provinces_data <- eez_meow %>% 
+  group_by(iso3, province) %>% 
+  summarize(a = 1) %>% 
+  ungroup() %>% 
+  group_by(province) %>% 
+  mutate(n_eez = n_distinct(iso3)) %>% 
   ungroup() %>% 
   mutate(province = fct_reorder(province, n_eez))
 
+province_map <- ggplot() +
+  geom_sf(data = provinces_data, aes(fill = province), color = "black", size = 0.1) +
+  geom_sf(data = coast, color = "black", size = 0.1) +
+  ggtheme_map() +
+  scale_fill_viridis_d() +
+  theme(legend.position = "none")
 
-# Barplots
-
-eezs_per_realm_plot <-
-  ggplot(eezs_per_realm, aes(x = realm, y = n_eez)) +
+province_bars <- provinces_data %>% 
+  st_drop_geometry() %>% 
+  select(province, n_eez) %>% 
+  distinct() %>% 
+  ggplot(aes(x = province, y = n_eez, fill = province)) +
   geom_col() +
   coord_flip() +
-  labs(x = "Realm", y = "Number of countries") +
-  guides(fill = F) +
+  labs(x = "Province", y = "Number of nations") +
   ggtheme_plot() +
-  scale_y_continuous(expand = c(0, 0)) +
+  theme(legend.position = "none") +
+  scale_fill_viridis_d() +
   geom_hline(yintercept = 2, linetype = "dashed", color = "black")
 
-eezs_per_province_plot <-
-  ggplot(eezs_per_province, aes(x = province, y = n_eez)) +
-  geom_col(color = "black") +
-  coord_flip() +
-  labs(x = "Province", y = "Number of countries") +
-  guides(fill = F) +
-  ggtheme_plot() +
-  scale_y_continuous(expand = c(0, 0)) +
-  geom_hline(yintercept = 2, linetype = "dashed", color = "black")
+province_segments <- plot_grid(province_map, province_bars,
+                               ncol = 1,
+                               rel_heights = c(5, 31.5))
+
+
+lazy_ggsave(plot = province_segments,
+            filename = "trading_units/province",
+            width = 10,
+            height = 32)
 
 # Export plots
-lazy_ggsave(plot = eezs_per_realm_plot,
-            filename = "eezs_per_realm",
-            width = 15, height = 8)
 
-# Export plots
-lazy_ggsave(plot = eezs_per_province_plot,
-            filename = "eezs_per_province",
-            width = 15, height = 20)
 
 # END OF SCRIPT #
 
