f945ab7abe7891a851c5f9a9953f2ec4a09c93fb
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2022-05-30 21:17:10 -0700

    Added HSI histogram

---
+++ scripts/03_descriptive_content/05_plot_hsi_histogram.R
@@ -0,0 +1,29 @@
+######################################################
+#title#
+######################################################
+# 
+# Draw a histogram of the Habitat Suitability Index
+#
+######################################################
+library(sta)
+library(here)
+library(tidyverse)
+
+master_cb <- readRDS(file = file.path(project_path, "processed_data", "master_costs_and_benefits.rds"))
+
+
+hsi_hist <- ggplot(data = master_cb,
+       mapping = aes(x = suitability)) +
+  geom_histogram(binwidth = 0.01,
+                 color = "black",
+                 fill = "steelblue") +
+  startR::ggtheme_plot() +
+  labs(x = "HSI",
+       y = "N",
+       subtitle = "N = 52,879")
+ 
+
+lazy_ggsave(hsi_hist,
+            "hsi_hist",
+            width = 15,
+            height = 7)
