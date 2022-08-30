e8dff486415ba8d97756e8553b566718ac005fab
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2022-08-30 09:57:49 -0700

    Updating two states map

--- scripts/05_figures/05_two_states_map_global.R
+++ scripts/05_figures/05_two_states_map_global.R
@@ -1,6 +1,17 @@
 
+library(rnaturalearth)
+library(tidyverse)
 
 # Load data
+base <- readRDS(
+  file = file.path(
+    project_path,
+    "processed_data",
+    "supply_curves",
+    "with_mpas",
+    "global_eez_supply_curves_with_mpas.rds"
+  ))
+
 eez_cb <-
   readRDS(
     file = file.path(
@@ -11,7 +22,7 @@ eez_cb <-
       "global_eez_supply_curves_with_mpas.rds"
     )) %>% 
   mutate(pixel_fraction = pmin(1 - ((tb - ((tb * 0.3) / pct)) / benefit), 1)) %>% 
-  filter(pixel_fraction >= 0) 
+  filter(pixel_fraction >= 0) %>% 
   select(lat, lon) %>% 
   mutate(bau = 1)
 
@@ -34,18 +45,24 @@ when <- full_join(eez_h_sum_cb, eez_cb, by = c("lat", "lon")) %>%
   replace_na(list("bau" = 0,
                   "mkt" = 0)) %>% 
   mutate(both = mkt + bau,
-         protected = case_when(mkt == 1 & bau == 1 ~ "Protected both ways",
-                              mkt == 0 & bau == 1 ~ "Protected only under BAU",
-                              mkt == 1 & bau == 0 ~ "Protected only under MKT")) %>% 
+         protected = case_when(mkt == 1 & bau == 1 ~ "Protected under both",
+                               mkt == 0 & bau == 1 ~ "Protected only under BAU",
+                               mkt == 1 & bau == 0 ~ "Protected only under MKT",
+                               T ~ "Not protected")) %>% 
   ggplot() +
   geom_sf(data = coast, color = "black", fill = "transparent") +
   geom_tile(aes(x = lon, y = lat, fill = protected)) +
-  scale_fill_viridis_d() +
+  scale_fill_brewer(palette = "Set2") +
   ggtheme_map() +
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom",
+        legend.title = element_blank())
 
 
 lazy_ggsave(plot = when,
             filename = "30_by_segment/two_states_map",
             width = 20,
             height = 10)
+
+
+
+
